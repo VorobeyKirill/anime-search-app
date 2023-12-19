dff3a421231ad1769cc39d1aa27a2f9108d344dd
Kiril Varabei <kiril_varabei@epam.com>
2023-12-19 22:36:14 +0100

    feat: add react-router library and router configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { SearchPage } from './pages/Search/SearchPage';
+import { DetailsPage } from './pages/Details/DetailsPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SearchPage />,
+  },
+  {
+    path: "/details/:id",
+    element: <DetailsPage />
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-reportWebVitals();
